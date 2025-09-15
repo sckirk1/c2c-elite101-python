c91cc50b96f2a0fff97c4ae7f828487180f37515
Samuel Kirk <70180845+sckirk1@users.noreply.github.com>
2025-09-15 22:54:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,19 @@
 """
 
 
-def get_user_name():
-    return input("Please enter your name: ")
+# def get_user_name():
+#     return input("Please enter your name: ")
 
-def greet_user(name):
-    print(f"Hello, {name}!")
+# def greet_user(name):
+#     print(f"Hello, {name}!")
 
-def main():
-    user_name = get_user_name()
-    greet_user(user_name)
+# def main():
+#     user_name = get_user_name()
+#     greet_user(user_name)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+
+print("Hello World!")
+
